c01ec068e686ea8dfe2b5f3336ced6dec72b2943
lakshmimanukonda <119383582+lakshmimanukonda@users.noreply.github.com>
2023-05-22 15:05:01 -0500

    jm/myb179
    added navigation between screens

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/screens/UserSignIn';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
--- src/screens/UserSignIn.js
+++ src/screens/UserSignIn.js
@@ -1,23 +1,23 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React, { useState, useCallback} from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, Dimensions, SafeAreaView, ScrollView } from 'react-native';
 
-const UserSignIn = () => {
+
+function UserSignIn({navigation}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isChecked, setIsChecked] = useState(false);
-
+    const { height } = Dimensions.get('window');
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
     };
-    const handleSignIn = useCallback(async () => {
+    const handleSignIn = () => {
         if (email.trim() === '' || password.trim() === '') {
             console.log('Email and password cannot be empty');
             return;
         }
-
         try {
             const baseUrl = 'https://localhost:44342/api/v1/';
             const url = `User/getUserDetails?UserEmail=${email}&UserPassword=${password}`;
@@ -29,6 +29,7 @@ const UserSignIn = () => {
 
                     if (responseJson.success) {
                         console.log('Login successful');
+                        navigation.navigate('Home');
                     } else {
                         console.log('Login failed');
                     }
@@ -39,92 +40,100 @@ const UserSignIn = () => {
         } catch (error) {
             console.error('Sign in failed:', error);
         }
-    }, [email, password]);
+    };
     return (
-        <View style={{ margin: 20, marginTop: 50, alignItems: 'center' }}>
-            <Text style={{ marginTop: 10, fontSize: 24, fontWeight: 'bold', color: '#525252' }}>
-                Welcome to MYB - Store!
-            </Text>
-            <Text style={{ marginTop: 10, fontSize: 16, color: '#525252' }}>
-                Your One Stop Indian Store
-            </Text>
+        <ScrollView>
+            <SafeAreaView style={{ flex: 1 }} >
+                <View style={{ flex: 1, margin: 20, marginTop: 10, alignItems: 'center' }}>
+                    <Text style={{ marginTop: height * 0.1, fontSize: 24, fontWeight: 'bold', color: '#525252' }}>
+                        Welcome to MYB - Store!
+                    </Text>
+                    <Image
+                        source={require('../assets/myBazar.jpg')}
+                        style={{ marginTop: 10, width: 100, height: 100 }}
+                    />
+                    <Text style={{ marginTop: 10, fontSize: 16, color: '#525252' }}>
+                        Your One Stop Indian Store
+                    </Text>
 
-            <TextInput
-                style={{
-                    marginTop: 30,
-                    width: '100%',
-                    height: 45,
-                    borderWidth: 1,
-                    borderColor: 'black',
-                    borderRadius: 8,
-                    paddingHorizontal: 10,
-                }}
-                placeholder="Email Address"
-                placeholderTextColor="#B6B7B7"
-                onChangeText={setEmail}
-            />
+                    <TextInput
+                        style={{
+                            marginTop: 40,
+                            width: '100%',
+                            height: 45,
+                            borderWidth: 1,
+                            borderColor: 'black',
+                            borderRadius: 8,
+                            paddingHorizontal: 10,
+                        }}
+                        placeholder="Email Address"
+                        placeholderTextColor="#B6B7B7"
+                        onChangeText={setEmail}
+                    />
 
-            <TextInput
-                style={{
-                    marginTop: 10,
-                    width: '100%',
-                    height: 45,
-                    borderWidth: 1,
-                    borderColor: 'black',
-                    borderRadius: 8,
-                    paddingHorizontal: 10,
-                }}
-                placeholder="Password"
-                placeholderTextColor="#B6B7B7"
-                onChangeText={setPassword}
-                secureTextEntry={true}
-            />
+                    <TextInput
+                        style={{
+                            marginTop: 20,
+                            width: '100%',
+                            height: 45,
+                            borderWidth: 1,
+                            borderColor: 'black',
+                            borderRadius: 8,
+                            paddingHorizontal: 10,
+                        }}
+                        placeholder="Password"
+                        placeholderTextColor="#B6B7B7"
+                        onChangeText={setPassword}
+                        secureTextEntry={true}
+                    />
 
-            <TouchableOpacity
-                style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}
-                onPress={handleCheckboxChange}
-            >
-                <View
-                    style={{
-                        width: 20,
-                        height: 20,
-                        borderWidth: 1,
-                        borderColor: 'black',
-                        marginRight: 5,
-                        backgroundColor: isChecked ? '#3D79B2' : 'transparent',
-                    }}
-                />
-                <Text>Remember Me</Text>
-            </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ marginTop: 20, flexDirection: 'row', alignItems: 'center' }}
+                        onPress={handleCheckboxChange}
+                    >
+                        <View
+                            style={{
+                                width: 20,
+                                height: 20,
+                                borderWidth: 1,
+                                borderColor: 'black',
+                                marginRight: 5,
+                                backgroundColor: isChecked ? '#3D79B2' : 'transparent',
+                            }}
+                        />
+                        <Text>Remember Me</Text>
+                    </TouchableOpacity>
 
-            <TouchableOpacity
-                style={{
-                    marginTop: 10,
-                    width: '100%',
-                    height: 40,
-                    backgroundColor: '#3D79B2',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 8,
-                }}
-                onPress={handleSignIn}
-            >
-                <Text style={{ color: '#F8F8F8', fontWeight: 'bold', fontSize: 16 }}>Login</Text>
-            </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            marginTop: 20,
+                            width: '100%',
+                            height: 40,
+                            backgroundColor: '#3D79B2',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            borderRadius: 8,
+                        }}
+                        onPress={handleSignIn}
+                    >
+                        <Text style={{ color: '#F8F8F8', fontWeight: 'bold', fontSize: 16 }}>Login</Text>
+                    </TouchableOpacity>
 
-            <TouchableOpacity style={{ marginTop: 10 }}>
-                <Text style={{ color: '#3D79B2', fontWeight: 'bold', fontSize: 16 }}>
-                    Forgot Password
-                </Text>
-            </TouchableOpacity>
+                    <TouchableOpacity style={{ marginTop: 20 }}>
+                        <Text style={{ color: '#3D79B2', fontWeight: 'bold', fontSize: 16 }}>
+                            Forgot Password
+                        </Text>
+                    </TouchableOpacity>
 
-            <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                <Text style={{ color: 'black' }}>Don't have an account?</Text>
-                <TouchableOpacity>
-                    <Text style={{ color: '#3D79B2', fontWeight: 'bold' }}>Register</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
+                    <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                        <Text style={{ color: 'black' }}>Don't have an account?</Text>
+                        <TouchableOpacity onPress={() => navigation.navigate('Register')} >
+                            <Text style={{ color: '#3D79B2', fontWeight: 'bold' }}>Register</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </ScrollView>
     );
 };
 
