27bac5aa38122b0200076ea103557af6983b0a57
sravanisd <119383483+sravanisd@users.noreply.github.com>
2023-06-11 23:35:58 -0500

    181 /Apple Signin
    181/Apple Signin

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ const App = () => {
         }
       
         Alert.alert(
-          `Identity Token: ${identityToken}\nEmail: ${email}\nFirst Name: ${firstName}\nLast Name: ${lastName}`
+          ` \nEmail: ${email}\nFirst Name: ${firstName}\nLast Name: ${lastName}`
         );
       });
       
