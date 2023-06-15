0278f8df52d747ce154e2b6b7ce65c84cc553eda
PraveenaDurgaBellam <119383631+PraveenaDurgaBellam@users.noreply.github.com>
2023-06-15 16:40:15 -0500

    pb/MYB-180 - Login With Gmail

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,27 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, Button, View, Alert } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import styles from './HomeScreenStyles';
 
-function HomeScreen (navigation){
+function HomeScreen ({navigation}){
+    const handleLogout = async () => {
+        try {
+          await GoogleSignin.revokeAccess();
+          await GoogleSignin.signOut();
+          Alert.alert('Log Out', 'Logged Out Successfully');
+          console.log('User signout successfully');
+          navigation.navigate('UserSignIn');
+        } catch (error) {
+          console.error('Error signing out:', error);
+        }
+    }
     return (
-        <SafeAreaView>
-            <Text>HomeScreen</Text>
+        <SafeAreaView style={styles.container}>
+        <View style={styles.centerContainer}>
+            <Text style={styles.homeText}>HomeScreen</Text>
+            <Button title="Log out" onPress={handleLogout} />
+        </View>
         </SafeAreaView>
     );
 };
